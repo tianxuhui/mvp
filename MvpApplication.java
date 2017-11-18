71ab1b48daa368854d5fcd2d31a35046580cb16f
tianxuhui <33782732+tianxuhui@users.noreply.github.com>
2017-11-18 20:38:15 +0800

    Add files via upload

---
+++ MvpApplication.java
@@ -0,0 +1,19 @@
+package com.example.tg.simplemvp;
+
+import android.app.Application;
+
+import cn.finalteam.okhttpfinal.OkHttpFinal;
+import cn.finalteam.okhttpfinal.OkHttpFinalConfiguration;
+
+/**
+ * Created by TG on 2017/11/18.
+ */
+
+public class MvpApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        OkHttpFinalConfiguration.Builder builder = new OkHttpFinalConfiguration.Builder();
+        OkHttpFinal.getInstance().init(builder.build());
+    }
+}
